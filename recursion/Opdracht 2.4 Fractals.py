a43e94d170fc752b9dc9621bbc5aac96bf788e37
Jelle Verstappen <jelle.verstappen@telenet.be>
2022-05-20 11:25:39 +0200

    finished

--- recursion/Opdracht 2.4 Fractals.py
+++ recursion/Opdracht 2.4 Fractals.py
@@ -5,9 +5,17 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
  
 """ Recursive fractal function """
-def recursive_fractal(0, 0, width, height, fractal_depth, screen):
+def recursive_fractal(x, y, width, height, fractal_depth, screen):
     if fractal_depth == 0:
-        return 
+        return
+    draw_shape(x, y, width, height, screen)
+    recursive_fractal(x, y, width/2, height/2, fractal_depth-1, screen)
+    recursive_fractal(x+width/2, y, width/2, height/2, fractal_depth-1, screen)
+    recursive_fractal(x, y+height/2, width/2, height/2, fractal_depth-1, screen)
+    recursive_fractal(x+width/2, y+height/2, width/2, height/2, fractal_depth-1, screen)
+
+
+
     
  
 """ Draw shape based on wanted position (x,y) and size (width, height) """
@@ -32,7 +40,7 @@ def draw_shape(x,y,width,height, screen):
 def main(): 
     # Init 
     running = True
-    width, height = 700, 700
+    width, height = 1920, 1080
     pygame.init()
     screen = pygame.display.set_mode([width, height])
     pygame.display.set_caption("My Game")
@@ -47,7 +55,7 @@ def main():
         screen.fill(WHITE)
 
         """ Should draw fractals of certain depth as indicated in GitHub """
-        fractal_depth = 3
+        fractal_depth = 10
         recursive_fractal(0, 0, width, height, fractal_depth, screen)
     
         pygame.display.flip()
--- recursion/Opdracht 2.5 Towers of Hanoi/hanoi.py
+++ recursion/Opdracht 2.5 Towers of Hanoi/hanoi.py
@@ -4,17 +4,28 @@ import pygame, time
 Recursive function to solve towers of Hanoi
 """
 def towers_of_hanoi(n , source, destination, extra):
-    pass
+    if n==1:
+        print ("Move disk 1 from source",source,"to destination",destination)
+        game_loop(n, source, destination)
+        return
+
+    towers_of_hanoi(n-1, source, extra, destination)
+    print ("Move disk",n,"from source",source,"to destination",destination)
+    game_loop(n, source, destination)
+    towers_of_hanoi(n-1, extra, destination, source)
+
+
+
          
 """
 Driver code
 """
 n_disks = 3
 
-# # To be used when visualising
-# from visualise_hanoi import *
-# make_disks(n_disks)
-# update_visuals()
-# time.sleep(2)
+# To be used when visualising
+from visualise_hanoi import *
+make_disks(n_disks)
+update_visuals()
+time.sleep(2)
 
 towers_of_hanoi(n_disks,'A','C','B')

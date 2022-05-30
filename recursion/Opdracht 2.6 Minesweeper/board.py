7faaff460b02cb74fa0185543b1ae9ae15552bf5
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2022-05-30 21:31:21 +0200

    Added display methods

--- recursion/Opdracht 2.6 Minesweeper/board.py
+++ recursion/Opdracht 2.6 Minesweeper/board.py
@@ -1,3 +1,60 @@
 from cell import Cell
 
 class Board():
+    def __init__(self, SQUARE_SIZE = 9):
+        self.grid = []
+
+        for i in range(SQUARE_SIZE):
+            row = []
+            for j in range(SQUARE_SIZE):
+                row.append(Cell("?"))
+            self.grid.append(row)
+
+
+    """ Show grid whilst playing the game """
+    def display_play_grid(self):
+        """ Header of display grid message"""
+        header = self.display_header("LET'S PLAY")
+        
+        """ Body of display grid message """
+        body = f""
+        for index, row in enumerate(self.grid):
+            body += f"{index}|"
+            for index, cell in enumerate(row):
+                if cell.is_covered == True:
+                    body += f"{self.CHAR_BLANK} "
+                else:
+                    body += f"{cell.neighbouring_mine_count} "
+            body+=f"\n"
+        print(header + body)
+
+    """ Show grid with locations of mines revealed """
+    def display_mines_grid(self):
+        """ Header of display mines message"""
+        header = self.display_header("Mine Placement")
+        """ Body of display mines message """
+        body = f""
+        for index, row in enumerate(self.grid):
+            body += f"{index}|"
+            for index, cell in enumerate(row):
+                body += f"{cell.display_character} "
+            body+=f"\n"
+
+        print(header + body)
+
+    """ Show grid with number of neighbouring mines revealed """
+    def display_neighbouring_mines_grid(self):
+        """ Header of display neighbours message"""
+        header = self.display_header("Neigbouring mines")
+        """ Body of display neighbours message """
+        body = f""
+        for index, row in enumerate(self.grid):
+            body += f"{index}|"
+            for index, cell in enumerate(row):
+                if cell.display_character==self.CHAR_MINE:
+                    body += f"{self.CHAR_MINE} "
+                else:
+                    body += f"{cell.neighbouring_mine_count} "
+            body+=f"\n"
+        print(header+body)
+
--- recursion/Opdracht 2.6 Minesweeper/cell.py
+++ recursion/Opdracht 2.6 Minesweeper/cell.py
@@ -1,1 +1,5 @@
 class Cell():
+    def __init__(self, display_character):
+        self.is_covered = True
+        self.display_character = display_character
+        self.neighbouring_mine_count = 0
